dd4f67ef1d08a603166b94b9c197e67f77c3e334
Michael Hood <michaelhood@gmail.com>
2014-01-25 07:19:35 -0800

    Create github-language-stats-text.user.js

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-github-language-stats-text.user.js
+GitHub: Language Stats in Text
 ==================================
 
-GitHub: Language Stats in Text
+Simple userscript to put the language's name in repos' language stats graphs. *(unofficial)*
+
+<p align="center"><img src="http://enhance.github.io/github-language-stats-text.user.js/images/demo-github-language-stats-text-arrows.png" width=802 height=88></p>
---
+++ github-language-stats-text.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       GitHub: Language Stats in Text
+// @namespace  http://enhance.github.io/github-language-stats-text.user.js
+// @version    0.1
+// @description  GitHub: Language Stats in Text
+// @match      *://github.com/*/*
+// @copyright  2014, mh
+// ==/UserScript==
+
+GM_addStyle(".repository-lang-stats-graph span { text-align: center; text-indent: 0px !important; color: rgba(255,255,255,0.8); text-transform: uppercase; font-size: xx-small; letter-spacing: 0.05em; }")
